9e5c3388f283c58d6d696df449484a85ad2d8b33
lyh <lyh@dibaidc.com>
2017-06-22 14:53:55 +0800

    初始化mvp项目

---
+++ mvp/app/src/main/java/operation/dibai/huyu/mvp/MainActivity.java
@@ -0,0 +1,13 @@
+package operation.dibai.huyu.mvp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
